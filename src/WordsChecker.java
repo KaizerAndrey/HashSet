f52d196c159d5731cbfadab68de0232b2d3dfc59
Andrey Kaizer <andre.kaizer@yandex.ru>
2022-10-13 22:54:17 +0300

    Реализация класса WordsChecke, демонстрация работы класса Main

--- src/WordsChecker.java
+++ src/WordsChecker.java
@@ -1,2 +1,18 @@
+import java.util.Collections;
+import java.util.HashSet;
+
 public class WordsChecker {
+
+    protected String inputText;
+    HashSet<String> text = new HashSet<>();
+
+    public WordsChecker(String inputText) {
+        this.inputText = inputText;
+        Collections.addAll(text, inputText.split("\\P{IsAlphabetic}+"));
+    }
+
+    protected boolean hasWord(String word) {
+        return text.contains(word);
+    }
+
 }
